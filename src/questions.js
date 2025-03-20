1e9b8995944e2bc84cc100b7712ce013c8d93796
andriakumbari <andrewkumbari@gmail.com>
2025-03-20 17:32:03 +0400

    questions

--- src/questions.js
+++ src/questions.js
@@ -1,5 +1,198 @@
-export const questions  = [
+export const data  = [
     {
-       
+      "title" : "HTML",
+      "questions": [
+        {
+            "questionNumber" : "1 out of 6",
+            "question" : "What does HTML stand for?",
+            "options" : [
+                "Hyper Training Markup Language",
+                "Hyper Text Marketing Language",
+                "Hyper Text Markup Language",
+                "Hyper Text Markup Leveler",
+            ],
+            "correctAnswerIndex" : 2,
+            "answer" : "Hyper Text Markup Language"
+        },
+        {
+            "questionNumber" : "2 out of 6",
+            "question" : "What is correct HTML element for largest heading?",
+            "options" : [
+                "<head>",
+                "<h6>",
+                "<header>",
+                "<h1>",
+            ],
+            "correctAnswerIndex" : 3,
+            "answer" : "<h1>"
+        },
+        {
+            "questionNumber" : "3 out of 6",
+            "question" : "What is correct HTML tag for creating a hyperlink?",
+            "options" : [
+                "<link>",
+                "<hyperlink>",
+                "<a>",
+                "<href>"
+            ],
+            "correctAnswerIndex" : 2
+        },
+        {
+            "questionNumber" : "4 out of 6",
+            "question" : "What is correct way of making a text input in HTML?",
+            "options" :[
+                "<input type='text'>",
+                "<i>",
+                "<text type='input'>",
+                "<type>"
+            ],
+            "correctAnswerIndex" : 0
+        },
+        {
+            "questionNumber" : "5 out of 6",
+            "question" : "How to make a comment in HTML?",
+            "options" :[
+                "<  ---  >",
+                "<comment><comment/>",
+                "<!-- -->",
+                "<html>"
+            ],
+            "correctAnswerIndex" : 2
+        },
+        {
+            "questionNumber" : "6 out of 6",
+            "question" : "Which tag inserts a single line break?",
+            "options" :[
+                "<break>",
+                "<bk>",
+                "!break!",
+                "<br>"
+            ],
+            "correctAnswerIndex" : 3
+        }
+      ]
+    },
+    {
+        "title": "CSS",
+        "questions" : [
+            {
+                "questionNumber" : "1 out of 4",
+                "question" : "What does CSS stand for?",
+                "options" : [
+                    "Cascading Style Sheets",
+                    "Cascading Scheme Sheets",
+                    "Colorful Style Sheets",
+                    "Colorful Sensing Sheets",
+                ],
+                "answer" : "Cascading Style Sheets",
+                "correctAnswerIndex" : 0
+            },
+            {
+                "questionNumber" : "2 out of 4",
+                "question" : "How to connect CSS with HTML?",
+                "options" : [
+                    "<link rel='stylesheet' href='style.css'>",
+                    "<CSS connect>",
+                    "<stylesheet src='style.css'>'",
+                    "<link style.css>",
+                ],
+                "correctAnswerIndex" : 0
+            },
+            {    
+                "questionNumber" : "3 out of 4",
+                "question" : "Which one is correct syntax?",
+                "options" : [
+                    "body-backgroundColor-red;",
+                    "[body] : backgroundColor-red;",
+                    "{body , backgroundColour:red;}",
+                    "body{backgroundColor:red;}",
+                ],
+                "correctAnswerIndex" : 3
+            },
+            {
+                "questionNumber" : "4 out of 4",
+                "question" : "How to make a comment in CSS?",
+                "options" : [
+                    "/ like this",
+                    "/!! like this !!/",
+                    "// like this",
+                    "/* like this */"
+                ],
+                "correctAnswerIndex" : 3
+            }    
+
+        ]
+    },
+    {
+        "title":'JS',
+        'questions':[
+            {
+                "questionNumber" : "1 out of 6",
+                "question" : "In which HTML element do you put Javascript in?",
+                "options" : [
+                    "<script>",
+                    "<JS>",
+                    "<java>",
+                    "<html>"
+                ],
+                "correctAnswerIndex" : 0
+            },
+            {
+                "questionNumber" : "2 out of 6",
+                "question" : "How to write 'Hello World' using alert box?",
+                "options" : [
+                    "alert(Hello World)",
+                    "<alert>Hello world<alert/>",
+                    "var alert = ('Hello World')",
+                    "alert('Hello World')"
+                ],
+                "correctAnswerIndex" : 3
+            },
+            {
+                "questionNumber" : "3 out of 6",
+                "question" : "How to create a function?",
+                "options" : [
+                    "function (MyFunction)",
+                    "()function MyFunction",
+                    "MyFunction () function",
+                    "function MyFunction()"
+                ],
+                "correctAnswerIndex" : 3
+            },
+            {
+                "questionNumber" : "4 out of 6",
+                "question" : "What is correct way of making an array?",
+                "options" : [
+                    "var fruits = {apple , pear , orange}",
+                    "fruits = (apple , pear , orange)",
+                    "var fruits = ('apple' , 'pear' , 'orange')",
+                    "var fruits = ['apple' , 'pear' , 'orange']"
+                ],
+                "correctAnswerIndex" : 3
+            },
+            {
+                "questionNumber" : "5 out of 6",
+                "question" : "How do you make an IF statment?",
+                "options" : [
+                    "if {condition} () ",
+                    "{if} condition ",
+                    "if(condition){}",
+                    "{if condition}()"
+                ],
+                "correctAnswerIndex" : 2
+            },
+            {
+                "questionNumber" : "6 out of 6",
+                "question" : "What will the code return : console.log('5'+1)",
+                "options" : [
+                    "6",
+                    "'5'1",
+                    "51",
+                    "'51'"
+                ],
+                "correctAnswerIndex" : 3
+            }
+        ]
     }
 ]
+
